057e096b281d2b447c0de5dd8ffa4f28910bbc33
Rodrigo Cardoso <rodrigo.ti@mfrural.com.br>
2018-05-14 17:56:04 -0300

    Models

--- appTeste/routes/web.php
+++ appTeste/routes/web.php
@@ -75,3 +75,5 @@ Route::get('home', function (){
     $livros = [];
    return view('home', compact('usuarios', 'livros'));
 });
+
+Route::get('usuario', ['uses' => 'UsuarioController@index', 'as' => 'usario']);
